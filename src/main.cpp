4099f5bb64c79fbb87ebba8a5d18fa4892cb8dfe
sami-hatna66 <sami.hatna2003@gmail.com>
2022-07-28 09:50:06 +0100

    Final Tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- *  Copyright (C) Codeplay Software Ltd.
+ *  Copyright 2022 (C) Codeplay Software Ltd.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  *
  *  Codeplay's ComputeCpp SDK
  *
- *  main.cpp.cpp
+ *  main.cpp
  *
  *  Description:
  *    SYCL implementation of Conway's Game of Life 
